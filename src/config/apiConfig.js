9f055164b056bbf69917fc861d9d99def9eb6db4
Jahanzaib Farooq <157870830+jahnzab@users.noreply.github.com>
2025-08-10 23:15:00 +0530

    Update apiConfig.js

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -2,6 +2,6 @@
 const API_BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:8000" // FastAPI backend locally
-    : "https://your-render-backend.onrender.com"; // Render API
+    : "https://hospital-backend-rvia.onrender.com"; // Render API
 
 export default API_BASE_URL;
