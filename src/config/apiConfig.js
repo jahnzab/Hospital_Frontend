10c6595d2cf4f3bc0556bc7a81026748cfcb1ab6
Jahanzaib Farooq <157870830+jahnzab@users.noreply.github.com>
2025-08-11 08:32:54 +0530

    Update apiConfig.js

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,7 +1,8 @@
 // src/config/apiConfig.js
 const API_BASE_URL =
   process.env.NODE_ENV === "development"
-    ? "http://localhost:8000" // FastAPI backend locally
-    : "https://hospital-backend-1-ygmp.onrender.com"; // Render API
+  ? "https://hospital-backend-1-ygmp.onrender.com"
+    : "http://localhost:8000" // FastAPI backend locally
+   
 
 export default API_BASE_URL;
