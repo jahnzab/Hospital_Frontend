0f395d7b45f9e2038d5601e38e0eada9af813b5b
Jahanzaib Farooq <157870830+jahnzab@users.noreply.github.com>
2025-08-13 19:56:17 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,6 +17,13 @@
 // src/pages/Home.js
 import React from "react";
 
+
+import himage from   "../heart.webp"
+import bimage from "../brain.webp";
+import gimage from  "../gastro.webp"
+import oimage from  "../ortho.webp"
+import "bootstrap/dist/css/bootstrap.min.css";
+
 export default function Home() {
   const backgroundStyle = {
     backgroundImage: "url('Doctor.jpg')",
@@ -39,7 +46,36 @@ export default function Home() {
     margin: "0 auto",
   };
 
+  const specialties = [
+  {
+    id: "cardiac",
+    title: "Cardiology",
+    text: "We've been at the forefront of heart care for 24+ years, combining advanced technology with expert treatment to deliver the best outcomes.",
+    image: himage,
+  },
+
+  {
+    id: "neuro",
+    title: "Neurology",
+    text: "We offer specialised care for a wide range of neurological disorders in adults & children and are equipped with advanced technologies.",
+    image: bimage,
+  },
+  {
+    id: "gastro",
+    title: "Gastro Sciences",
+    text: "We're one of the leading gastroenterology hospitals in India, specialising in treating digestive & liver-related diseases in adults & kids.",
+    image: gimage,
+  },
+  {
+    id: "ortho",
+    title: "Orthopaedics",
+    text: "As leaders in orthopaedics, we offer care for bone, joint & spine issues, supported by a team of experts and state-of-the-art facilities.",
+    image: oimage,
+  },
+];
+
   return (
+    <>
     <div style={backgroundStyle}>
       <div style={overlayStyle}>
         <h2>Welcome to the Hospital Booking System</h2>
@@ -49,5 +85,32 @@ export default function Home() {
         </p>
       </div>
     </div>
+    <section className="container my-5">
+  <h2 className="text-center mb-4">Our Centres of Excellence</h2>
+  <div className="row">
+    {specialties.map((spec) => (
+      <div
+        key={spec.id}
+        className="col-12 col-sm-6 col-lg-4 mb-4"
+        onClick={() => handleClick(spec.id)}
+      >
+        <div
+          className="excellence-card card text-center"
+          style={{
+            backgroundImage: url(${spec.image}),
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          <div className="card-body">
+            <h5 className="card-title">{spec.title}</h5>
+            <p className="card-text">{spec.text}</p>
+          </div>
+        </div>
+      </div>
+    ))}
+  </div>
+</section>
+    </>
   );
 }
