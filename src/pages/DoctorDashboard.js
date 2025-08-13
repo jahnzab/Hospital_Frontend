f31388dca1dba4271f032100e90296c3c127824d
Jahanzaib Farooq <157870830+jahnzab@users.noreply.github.com>
2025-08-13 20:45:23 +0530

    Update DoctorDashboard.js

--- src/pages/DoctorDashboard.js
+++ src/pages/DoctorDashboard.js
@@ -90,7 +90,7 @@ export default function App() {
         setLoading(false);
       }
     }
-    if (doctorToken) fetchPatientsToday();
+    if (doctortoken) fetchPatientsToday();
   }, [doctortoken]);
 
   function handleInputChange(e) {
